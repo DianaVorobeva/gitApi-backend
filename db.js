e77c830f547d0dbf2b70b3bdd3dc2d47c59a8ae4
DianaVorobeva <Diana@MacBook-Pro-Aleksej.local>
2024-05-23 19:36:24 +0300

    change name of env vars

--- db.js
+++ db.js
@@ -4,13 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
  const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD,
+    process.env.PGDATABASE , 
+    process.env.POSTGRES_USER, 
+    process.env.PGPASSWORD,
     {
         dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        host: process.env.PGHOST,
+        port: process.env.PGPORT
     }
 )
 export default sequelize;
